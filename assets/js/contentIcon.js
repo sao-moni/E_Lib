c8d3f6137a930c0d2f07bbf80350c7a63fdd066b
sao-moni <keomony074@gmail.com>
2026-08-18 07:56:02 +0000

    update read

--- assets/js/contentIcon.js
+++ assets/js/contentIcon.js
@@ -1,16 +1,6 @@
 /*create vari */
-const contentBtn = document.getElementById('contentBtn');
+const contentBtn = document.querySelectorAll('#contentBtn');
 let getScrollPosition = window.pageYOffset
 const contentBar= document.getElementById('contentBar')
-window.addEventListener('scroll', (e)=>{
-    e.preventDefault
-    let getCurrentScrollPosition = window.pageYOffset
-    if ( getCurrentScrollPosition<getScrollPosition){
-        contentBar.style.bottom='1%'
-        getScrollPosition=getCurrentScrollPosition
-    }
-    else{
-        contentBar.style.bottom=''
-    }
-})
-/*Still in devlopment */
+
+/*Still in devlopment for the content bar progcess to work well */
