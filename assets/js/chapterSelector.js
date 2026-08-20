f13943a4214a5cd9dd9a92e7226f7961820e3a6f
sao-moni <keomony074@gmail.com>
2026-08-20 07:31:04 +0000

    resync

--- assets/js/chapterSelector.js
+++ assets/js/chapterSelector.js
@@ -17,12 +17,27 @@ if (sidebarLinks) {
             link.style.fontWeight = 'bold';
             const modifedHref = href.replace('#', '')
             const getChapter = chapterContent.filter((chapter) => chapter.chapter === modifedHref)
+            var globalChapter = getChapter
             chapterTitle.innerHTML = getChapter.map((title) => title.chapter)
             content.innerHTML = getChapter.map((content) => content.content)
-            activeCri.forEach()
         });
     });
 }
 /* Still in Devlopment */
 
 /*New code under contruction */
+/*create vari */
+const contentBtn = document.querySelectorAll('#contentBtn');
+const contentBar= document.getElementById('contentBar')
+const contentChapterBar = document.getElementById('contentBarChapter')
+if(contentBtn){
+    contentBtn.forEach(btn=>{
+        btn.addEventListener('click',(e)=>{
+            e.preventDefault
+            const chapterNum=1
+            chapterNum=chapterNum + 1
+            console.log(chapterNum)
+        })
+    })
+}
+/*Still in devlopment for the content bar progcess to work well */
--- assets/js/contentIcon.js
+++
@@ -1,6 +0,0 @@
-/*create vari */
-const contentBtn = document.querySelectorAll('#contentBtn');
-let getScrollPosition = window.pageYOffset
-const contentBar= document.getElementById('contentBar')
-
-/*Still in devlopment for the content bar progcess to work well */
